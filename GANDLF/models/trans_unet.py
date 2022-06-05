c0b78572e1e31658334970a8029a7caea52e002e
Ekaterina Sedykh <58919166+katesedykh@users.noreply.github.com>
2022-06-05 22:27:26 +0300

    upd transunet

--- GANDLF/models/trans_unet.py
+++ GANDLF/models/trans_unet.py
@@ -1,3 +1,4 @@
+#TransUNet official implementation: https://github.com/Beckschen/TransUNet
 import copy
 import math
 import torch
@@ -5,7 +6,116 @@ import torch.nn as nn
 import numpy as np
 from torch.nn.modules.utils import _pair
 from .modelBase import ModelBase
+from collections import OrderedDict
+import torch.nn.functional as F
 
+
+class StdConv2d(nn.Conv2d):
+
+    def forward(self, x):
+        w = self.weight
+        v, m = torch.var_mean(w, dim=[1, 2, 3], keepdim=True, unbiased=False)
+        w = (w - m) / torch.sqrt(v + 1e-5)
+        return F.conv2d(x, w, self.bias, self.stride, self.padding,
+                        self.dilation, self.groups)
+
+def conv3x3(cin, cout, stride=1, groups=1, bias=False):
+    return StdConv2d(cin, cout, kernel_size=3, stride=stride,
+                     padding=1, bias=bias, groups=groups)
+
+def conv1x1(cin, cout, stride=1, bias=False):
+    return StdConv2d(cin, cout, kernel_size=1, stride=stride,
+                     padding=0, bias=bias)
+
+
+class PreActBottleneck(nn.Module):
+    """Pre-activation (v2) bottleneck block.
+    """
+
+    def __init__(self, cin, cout=None, cmid=None, stride=1):
+        super().__init__()
+        cout = cout or cin
+        cmid = cmid or cout//4
+
+        self.gn1 = nn.GroupNorm(32, cmid, eps=1e-6)
+        self.conv1 = conv1x1(cin, cmid, bias=False)
+        self.gn2 = nn.GroupNorm(32, cmid, eps=1e-6)
+        self.conv2 = conv3x3(cmid, cmid, stride, bias=False)  # Original code has it on conv1!!
+        self.gn3 = nn.GroupNorm(32, cout, eps=1e-6)
+        self.conv3 = conv1x1(cmid, cout, bias=False)
+        self.relu = nn.ReLU(inplace=True)
+
+        if (stride != 1 or cin != cout):
+            # Projection also with pre-activation according to paper.
+            self.downsample = conv1x1(cin, cout, stride, bias=False)
+            self.gn_proj = nn.GroupNorm(cout, cout)
+
+    def forward(self, x):
+
+        # Residual branch
+        residual = x
+        if hasattr(self, 'downsample'):
+            residual = self.downsample(x)
+            residual = self.gn_proj(residual)
+
+        # Unit's branch
+        y = self.relu(self.gn1(self.conv1(x)))
+        y = self.relu(self.gn2(self.conv2(y)))
+        y = self.gn3(self.conv3(y))
+
+        y = self.relu(residual + y)
+        return y
+
+
+class ResNetV2(nn.Module):
+    """Implementation of Pre-activation (v2) ResNet mode."""
+
+    def __init__(self, block_units, width_factor):
+        super().__init__()
+        width = int(64 * width_factor)
+        self.width = width
+
+        self.root = nn.Sequential(OrderedDict([
+            ('conv', StdConv2d(3, width, kernel_size=7, stride=2, bias=False, padding=3)),
+            ('gn', nn.GroupNorm(32, width, eps=1e-6)),
+            ('relu', nn.ReLU(inplace=True)),
+            #('pool', nn.MaxPool2d(kernel_size=3, stride=2, padding=0))
+        ]))
+
+        self.body = nn.Sequential(OrderedDict([
+            ('block1', nn.Sequential(OrderedDict(
+                [('unit1', PreActBottleneck(cin=width, cout=width*4, cmid=width))] +
+                [(f'unit{i:d}', PreActBottleneck(cin=width*4, cout=width*4, cmid=width)) for i in range(2, block_units[0] + 1)],
+                ))),
+            ('block2', nn.Sequential(OrderedDict(
+                [('unit1', PreActBottleneck(cin=width*4, cout=width*8, cmid=width*2, stride=2))] +
+                [(f'unit{i:d}', PreActBottleneck(cin=width*8, cout=width*8, cmid=width*2)) for i in range(2, block_units[1] + 1)],
+                ))),
+            ('block3', nn.Sequential(OrderedDict(
+                [('unit1', PreActBottleneck(cin=width*8, cout=width*16, cmid=width*4, stride=2))] +
+                [(f'unit{i:d}', PreActBottleneck(cin=width*16, cout=width*16, cmid=width*4)) for i in range(2, block_units[2] + 1)],
+                ))),
+        ]))
+
+    def forward(self, x):
+        features = []
+        b, c, in_size, _ = x.size()
+        x = self.root(x)
+        features.append(x)
+        x = nn.MaxPool2d(kernel_size=3, stride=2, padding=0)(x)
+        for i in range(len(self.body)-1):
+            x = self.body[i](x)
+            right_size = int(in_size / 4 / (i+1))
+            if x.size()[2] != right_size:
+                pad = right_size - x.size()[2]
+                assert pad < 3 and pad > 0, "x {} should {}".format(x.size(), right_size)
+                feat = torch.zeros((b, x.size()[1], right_size, right_size), device=x.device)
+                feat[:, :, 0:x.size()[2], 0:x.size()[3]] = x[:]
+            else:
+                feat = x
+            features.append(feat)
+        x = self.body[-1](x)
+        return x, features[::-1]
 
 class Attention(nn.Module):
     def __init__(self, vis):
@@ -85,12 +195,16 @@ class Embeddings(ModelBase):
     def __init__(self, img_size, parameters: dict):
         super(Embeddings, self).__init__(parameters)
         img_size = _pair(img_size)
+         
+        grid_size = (32, 32)
+        small_patch_size = (img_size[0] // 16 // grid_size[0], img_size[1] // 16 // grid_size[1])
+        patch_size_real = (small_patch_size[0] * 16, small_patch_size[1] * 16)
+        n_patches = (img_size[0] // patch_size_real[0]) * (img_size[1] // patch_size_real[1])
+   
+        self.hybrid_model = ResNetV2(block_units= (3, 4, 9), width_factor=1)
+        in_channels = self.hybrid_model.width * 16
 
-
-        small_patch_size = _pair((16, 16)) #16,16
-        n_patches = (img_size[0] // small_patch_size[0]) * (img_size[1] // small_patch_size[1]) #1024
-
-        self.patch_embeddings = self.Conv(in_channels=self.n_channels,
+        self.patch_embeddings = self.Conv(in_channels=in_channels,
                                        out_channels=768,
                                        kernel_size=small_patch_size,
                                        stride=small_patch_size)
@@ -100,7 +214,7 @@ class Embeddings(ModelBase):
 
 
     def forward(self, x):
-        features = None
+        x, features = self.hybrid_model(x)
         x = self.patch_embeddings(x)  # (B, hidden. n_patches^(1/2), n_patches^(1/2))
         x = x.flatten(2)
         x = x.transpose(-1, -2)  # (B, n_patches, hidden)
@@ -175,10 +289,9 @@ class DecoderBlock(ModelBase):
         super().__init__(parameters)
                 
         self.up = nn.UpsamplingBilinear2d(scale_factor=2)
-
         
         self.double_conv = nn.Sequential(
-            self.Conv(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            self.Conv(in_channels + skip_channels, out_channels, kernel_size=3, padding=1, bias=False),
             self.Norm(out_channels),
             nn.ReLU(inplace=True),
             self.Conv(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
@@ -209,9 +322,8 @@ class DecoderCup(ModelBase):
         decoder_channels = (self.base_filters * 16, self.base_filters * 8, self.base_filters * 4, self.base_filters) #256,128,64,16
         in_channels = [head_channels] + list(decoder_channels[:-1])
         out_channels = decoder_channels
-        self.n_skip = 0
-        skip_channels=[0,0,0,0]
-
+        self.n_skip = 3
+        skip_channels=[self.base_filters * 32,self.base_filters * 16,self.base_filters * 4, 0] #[512,256,64,0]
         blocks = [
             DecoderBlock(in_ch, out_ch, sk_ch, parameters=parameters) for in_ch, out_ch, sk_ch in zip(in_channels, out_channels, skip_channels)
         ]
@@ -228,13 +340,14 @@ class DecoderCup(ModelBase):
                 skip = features[i] if (i < self.n_skip) else None
             else:
                 skip = None
+            
             x = decoder_block(x, skip=skip)
         return x
 
 
-class TransUNet(ModelBase):
+class TransUNet_hybrid(ModelBase):
     def __init__(self,  parameters: dict):
-        super(TransUNet, self).__init__(parameters)
+        super(TransUNet_hybrid, self).__init__(parameters)
         self.zero_head = False
         self.classifier = 'seg'
         self.transformer = Transformer(img_size=self.patch_size, vis = False, parameters=parameters)
